f637f0bf9f04bcc0d3a529cd8576790bffdc28fd
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2019-05-25 21:59:46 +0800

    add read sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <QCoreApplication>
+#include <QVariant>
+#include <QDebug>
 
 #include "xlsxdocument.h"
 #include "xlsxchartsheet.h"
@@ -8,14 +10,36 @@
 #include "xlsxworkbook.h"
 using namespace QXlsx;
 
+void write()
+{
+    // [1]  Writing excel file(*.xlsx)
+    QXlsx::Document xlsxW;
+    xlsxW.write("A1", "Hello Qt!"); // write "Hello Qt!" to cell(A,1). it's shared string.
+    xlsxW.saveAs("Test.xlsx"); // save the document as 'Test.xlsx'
+}
+
+void read()
+{
+    // [2] Reading excel file(*.xlsx)
+    Document xlsxR("Test.xlsx");
+    if (xlsxR.load()) // load excel file
+    {
+        int row = 1; int col = 1;
+        Cell* cell = xlsxR.cellAt(row, col); // get cell pointer.
+        if ( cell != nullptr )
+        {
+            QVariant var = cell->readValue(); // read cell value (number(double), QDateTime, QString ...)
+            qDebug() << var; // display value. it is 'Hello Qt!'.
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QXlsx::Document xlsx;
-    xlsx.write("A1", "Hello Qt!"); // write "Hello Qt!" to cell(A,1). it's shared string.
-    xlsx.saveAs("Test.xlsx"); // save the document as 'Test.xlsx'
+    write();
+    read();
 
-//    return a.exec();
     return 0;
 }
